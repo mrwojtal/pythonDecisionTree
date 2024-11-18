abff6df3bff937a18beaa1049f3264ec8a27f461
mrwojtal <wojtalk01@gmail.com>
2024-11-18 18:24:57 +0100

    Added comparing implemented DecisionTree algorithm with built in sklearn library DecisionTree classifier.

--- irisTest.py
+++ irisTest.py
@@ -1,39 +1,61 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn import tree
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from decisionTree import Tree
 
+def accuracy(y_test, y_pred):
+    return np.sum(y_test == y_pred)/len(y_test)
+
 data = datasets.load_iris()
 X, y = data.data, data.target
-#print(X)
-#print(y)
-#print(data.target_names)
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=44
 )
 
-classifier = Tree()
-classifier.fit(X_train, y_train)
-predictions = classifier.predict(X_test)
+#implemented clasifier
+my_classifier = Tree()
+my_classifier.fit(X_train, y_train)
+my_predictions = my_classifier.predict(X_test)
 
-def accuracy(y_test, y_pred):
-    return np.sum(y_test == y_pred)/len(y_test)
+#sklearn clasifier
+sklearn_classifier = tree.DecisionTreeClassifier()
+sklearn_classifier.fit(X_train, y_train)
+sklearn_predictions = sklearn_classifier.predict(X_test)
 
-acc = accuracy(y_test, predictions)
-print(acc)
+my_acc = accuracy(y_test, my_predictions)
+print(my_acc)
 
 print(
-    f"Classification report for classifier {classifier}:\n"
-    f"{classification_report(y_test, predictions)}\n"
+    f"Classification report for classifier {my_classifier}:\n"
+    f"{classification_report(y_test, my_predictions)}\n"
 )
 
 labels = ["setosa","versicolor","virginica"]
-cm_decisionTree = confusion_matrix(y_test, predictions, labels=[0,1,2])
+cm_decisionTree = confusion_matrix(y_test, my_predictions, labels=[0,1,2])
 disp_confusion_matrix = ConfusionMatrixDisplay(confusion_matrix=cm_decisionTree, display_labels=labels)
 disp_confusion_matrix.plot()
+title = "Confusion Matrix for implemented DecisionTree algorithm, accuracy: {acc:.2f}"
+title = title.format(acc = my_acc)
+plt.title(title)
+
+sklearn_acc = accuracy(y_test, sklearn_predictions)
+print(sklearn_acc)
+
+print(
+    f"Classification report for classifier {sklearn_classifier}:\n"
+    f"{classification_report(y_test, sklearn_predictions)}\n"
+)
+
+labels = ["setosa","versicolor","virginica"]
+cm_decisionTree = confusion_matrix(y_test, sklearn_predictions, labels=[0,1,2])
+disp_confusion_matrix = ConfusionMatrixDisplay(confusion_matrix=cm_decisionTree, display_labels=labels)
+disp_confusion_matrix.plot()
+title = "Confusion Matrix for sklearn library DecisionTree algorithm, accuracy: {acc:.2f}"
+title = title.format(acc = sklearn_acc)
+plt.title(title)
 plt.show()
--- mnistTest.py
+++ mnistTest.py
@@ -1,37 +1,62 @@
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn import tree
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 from decisionTree import Tree
 
+def accuracy(y_test, y_pred):
+    return np.sum(y_test == y_pred)/len(y_test)
+
 digits = datasets.load_digits()
 n_samples = len(digits.images)
 data = digits.images.reshape((n_samples, -1))
-
 X_train, X_test, y_train, y_test = train_test_split(
     data, digits.target, test_size=0.3, random_state=45
 )
 
-classifier = Tree()
-classifier.fit(X_train, y_train)
-predictions = classifier.predict(X_test)
+#implemented clasifier
+my_classifier = Tree()
+my_classifier.fit(X_train, y_train)
+my_predictions = my_classifier.predict(X_test)
 
-def accuracy(y_test, y_pred):
-    return np.sum(y_test == y_pred)/len(y_test)
+#sklearn classifier
+sklearn_classifier = tree.DecisionTreeClassifier()
+sklearn_classifier.fit(X_train, y_train)
+sklearn_predictions = sklearn_classifier.predict(X_test)
 
-acc = accuracy(y_test, predictions)
-print(acc)
+my_acc = accuracy(y_test, my_predictions)
+print(my_acc)
 
 print(
-    f"Classification report for classifier {classifier}:\n"
-    f"{classification_report(y_test, predictions)}\n"
+    f"Classification report for classifier {my_classifier}:\n"
+    f"{classification_report(y_test, my_predictions)}\n"
 )
 
 labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-cm_decisionTree = confusion_matrix(y_test, predictions, labels=labels)
+cm_decisionTree = confusion_matrix(y_test, my_predictions, labels=labels)
 disp_confusion_matrix = ConfusionMatrixDisplay(confusion_matrix=cm_decisionTree, display_labels=labels)
 disp_confusion_matrix.plot()
+title = "Confusion Matrix for implemented DecisionTree algorithm, accuracy: {acc:.2f}"
+title = title.format(acc = my_acc)
+plt.title(title)
+
+sklearn_acc = accuracy(y_test, sklearn_predictions)
+print(sklearn_acc)
+
+print(
+    f"Classification report for classifier {sklearn_classifier}:\n"
+    f"{classification_report(y_test, sklearn_predictions)}\n"
+)
+
+labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+cm_decisionTree = confusion_matrix(y_test, sklearn_predictions, labels=labels)
+disp_confusion_matrix = ConfusionMatrixDisplay(confusion_matrix=cm_decisionTree, display_labels=labels)
+disp_confusion_matrix.plot()
+title = "Confusion Matrix for sklearn library DecisionTree algorithm, accuracy: {acc:.2f}"
+title = title.format(acc = sklearn_acc)
+plt.title(title)
 plt.show()
